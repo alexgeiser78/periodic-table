2fafd52622fa2a8eaf761c08195e21ec8c78c4a2
Pleiades <113288425+alexgeiser78@users.noreply.github.com>
2026-02-28 23:46:37 +0100

    Add script to retrieve element information from database

---
+++ element.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+ELEMENT=$1
+
+# Vérifie si l'argument est un nombre (numéro atomique)
+if [[ $ELEMENT =~ ^[0-9]+$ ]]
+then
+  ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+  FROM elements e
+  JOIN properties p ON e.atomic_number = p.atomic_number
+  JOIN types t ON p.type_id = t.type_id
+  WHERE e.atomic_number=$ELEMENT;")
+else
+  ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+  FROM elements e
+  JOIN properties p ON e.atomic_number = p.atomic_number
+  JOIN types t ON p.type_id = t.type_id
+  WHERE e.symbol='$ELEMENT' OR e.name='$ELEMENT';")
+fi
+
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT_INFO"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
